1252ab296f98a3fda944818ca1c069954a0921c1
dgkanatsios <dgkanatsios@outlook.com>
2018-07-17 15:58:20 +0300

    change on sampleadd

--- functions/sampleadd/index.js
+++ functions/sampleadd/index.js
@@ -6,13 +6,15 @@ module.exports = function (context, req) {
     const now = new Date();
     let promises = [];
 
+    //this message is only visible today
     promises.push(storagehelpers.insertIntoTable({
         title: "title1",
         message: "message1",
-        from: now,
-        to: new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours() + 2, now.getUTCMinutes(), now.getUTCSeconds())
+        from: new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 0, 0, 0),
+        to: new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1, 0, 0, 0)
     }));
 
+    //this message will always be visible
     promises.push(storagehelpers.insertIntoTable({
         title: "title2",
         message: "message2"
